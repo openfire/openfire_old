f981e3e2f5f90bb2bf80ccafd4066599f744e5d5
Sam Gammon <sam@openfi.re>
2012-06-17 22:56:44 -0700

    Further development on the project page.

--- app/openfire/handlers/project.py
+++ app/openfire/handlers/project.py
@@ -1,5 +1,9 @@
 # -*- coding: utf-8 -*-
+from openfire.models import user
+from openfire.models import project
 from openfire.handlers import WebHandler
+
+from google.appengine.ext import ndb
 
 
 class ProjectLanding(WebHandler):
@@ -22,5 +26,27 @@ class ProjectHome(WebHandler):
 
         ''' Render project_home.html. '''
 
-        self.render('projects/project_home.html', project_slug=customurl)
+        p = project.Project.get_by_id(customurl)
+        if p is None:
+            ## make up a project real quick
+            p = project.Project(**{
+
+                    'key': ndb.Key(project.Project, 'fatcatmap'),
+                    'slug': 'fatcatmap',
+                    'name': 'fat cat map',
+                    'status': 'o',
+                    'proposal': ndb.Key(project.Proposal, 'cool'),
+                    'category': ndb.Key(project.Category, 'politics'),
+                    'summary': 'ever wonder who your local congressman gets his money from? fatcatmap answers that question, with data.',
+                    'pitch': 'Cool beans pitch.',
+                    'tech': 'Google App Engine, Amazon Web Services, Layer9, AppTools',
+                    'keywords': ['politics', 'influence', 'money', 'corruption', 'congress'],
+                    'creator': ndb.Key(user.User, 'sam'),
+                    'owners': [ndb.Key(user.User, 'sam'), ndb.Key(user.User, 'david')],
+                    'public': True,
+                    'viewers': []
+
+                })
+
+        self.render('projects/project_home.html', project_slug=customurl, project=p)
         return
