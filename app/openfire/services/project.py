119c5f7445a59a2084b960fcffcf9e0a019d1eb4
Ethan Leland <Ethan.Leland@gmail.com>
2012-06-29 17:15:53 -0700

    Fixing a merge issue

--- app/openfire/services/project.py
+++ app/openfire/services/project.py
@@ -140,12 +140,7 @@ class ProjectService(RemoteService):
 
         ''' Follow a project and return the new follow count. '''
 
-<<<<<<< HEAD
         return Echo(message='cool')
-=======
-        return Echo(message='')
-
->>>>>>> 33f69f093df6ddd0fc5711db2f7b5deeb135d7e8
 
     @remote.method(common_messages.FollowersRequest, common_messages.FollowersResponse)
     def followers(self, request):
@@ -167,10 +162,6 @@ class ProjectService(RemoteService):
         ''' Become a backer of a project. '''
 
         return Echo(message='')
-<<<<<<< HEAD
-=======
-
->>>>>>> 33f69f093df6ddd0fc5711db2f7b5deeb135d7e8
 
     @remote.method(project_messages.SuspendProject, Echo)
     def suspend(self, request):
@@ -186,11 +177,6 @@ class ProjectService(RemoteService):
 
         project.status = 'p'
         project.put()
-<<<<<<< HEAD
-=======
-
-        return Echo(message='Project suspended')
->>>>>>> 33f69f093df6ddd0fc5711db2f7b5deeb135d7e8
 
         return Echo(message='Project suspended')
 
