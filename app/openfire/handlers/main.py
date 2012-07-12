3ed1fab0ea8377f3699330d200b4781ee92c99c3
Sam Gammon <sam@openfi.re>
2012-07-12 16:00:57 -0700

    Fixes for landing avatars and a little tweak or two to the fixture

--- app/openfire/handlers/main.py
+++ app/openfire/handlers/main.py
@@ -2,6 +2,7 @@
 from openfire.models import user
 from openfire.models import assets
 from openfire.models import project as p
+from openfire.models import activity as a
 from google.appengine.ext import ndb
 from openfire.handlers import WebHandler
 from openfire.handlers.project import ProjectHome
@@ -13,74 +14,84 @@ class Landing(WebHandler):
     ''' openfire landing page. '''
 
     projects_per_page = 5
+    activity_per_page = 10
 
     def get(self):
 
         ''' Render landing.html or landing_noauth.html. '''
 
-        import pdb; pdb.set_trace()
+        ## fully cached page context
+        context = self.api.memcache.get('landing_page_context')
+        if not context:
 
-        ## fetch projects
-        pq = p.Project.query().order(p.Project.name)
+            ## fetch projects
+            pq = p.Project.query().order(p.Project.name)
+            aq = a.Activity.query().order(a.Activity.modified)
 
-        if not self.projects_per_page:
-            pc = pq.count()
-        else:
-            pc = self.projects_per_page
+            ## resolve project limit
+            if not self.projects_per_page:
+                pc = pq.count()
+            else:
+                pc = self.projects_per_page
 
-        context = {}
+            ## resolve activity limit
+            if not self.activity_per_page:
+                ac = aq.count()
+            else:
+                ac = self.activity_per_page
 
-        if pc > 0:
-            k_projects = pq.fetch(pc, keys_only=True)
+            ## start the new context
+            context = {}
 
-            ## prepare media (fetch avatars, videos + assets for both)
-            _assets, avatars, videos, projects = [], [], [], {}
-            data = ndb.get_multi(k_projects, use_cache=True, use_memcache=True, use_datastore=True)
-            for key, entity in ((entity.key, entity) for entity in data if entity is not None):
+            ## pull projects if there's more than one to pull
+            if pc > 0:
+                k_projects = pq.fetch(pc, keys_only=True)
 
-                # create the project context entry
-                projects[key.id()] = entity.to_dict()
-                projects[key.id()]['model'] = entity
-                projects[key.id()]['slug'] = entity.get_custom_url()
+                ## prepare media (fetch avatars, videos + assets for both)
+                _assets, avatars, videos, projects = [], [], [], {}
+                data = ndb.get_multi(k_projects, use_cache=True, use_memcache=True, use_datastore=True)
+                for key, entity in ((entity.key, entity) for entity in data if entity is not None):
 
-                # build a list of keys to pull, for avatars, videos, and assets
-                avatars.append(entity.avatar)
-                videos.append(entity.video)
-                _assets.append(ndb.Key(urlsafe=entity.avatar.id()))
+                    # create the project context entry
+                    projects[key.id()] = entity.to_dict()
+                    projects[key.id()]['model'] = entity
+                    projects[key.id()]['slug'] = entity.get_custom_url()
 
-            ## batch pull media
-            asset_keys = [k for k in (avatars + _assets + videos) if isinstance(k, (ndb.Key, ndb.Model))]
-            project_assets = ndb.get_multi(asset_keys, use_cache=True, use_memcache=True, use_datastore=True)
+                    # build a list of keys to pull, for avatars, videos, and assets
+                    avatars.append(entity.avatar)
+                    videos.append(entity.video)
+                    _assets.append(ndb.Key(urlsafe=entity.avatar.id()))
 
-            ## parse out query results
-            _assets = {}
-            for _key, _asset in zip(asset_keys, project_assets):
-                if _asset is None:
-                    continue
-                else:
-                    if isinstance(_asset, assets.Asset):
-                        _assets[_key] = _asset
-                    elif isinstance(_asset, assets.Avatar):
-                        projects[_asset.key.parent().id()]['avatar'] = _asset.to_dict()
-                        projects[_asset.key.parent().id()]['avatar']['model'] = _asset
-                    elif isinstance(_asset, assets.Video):
-                        projects[_asset.key.parent().id()]['video'] = _asset
+                ## batch pull media
+                asset_keys = [k for k in (avatars + _assets + videos) if isinstance(k, (ndb.Key, ndb.Model))]
+                project_assets = ndb.get_multi(asset_keys, use_cache=True, use_memcache=True, use_datastore=True)
 
-            ## build project structs
-            for key_id, project in projects.items():
+                ## parse out query results
+                _assets = {}
+                for _key, _asset in zip(asset_keys, project_assets):
+                    if _asset is None:
+                        continue
+                    else:
+                        if isinstance(_asset, assets.Asset):
+                            _assets[_key.urlsafe()] = _asset
+                        elif isinstance(_asset, assets.Avatar):
+                            projects[_asset.key.parent().id()]['avatar'] = _asset.to_dict()
+                            projects[_asset.key.parent().id()]['avatar']['model'] = _asset
+                        elif isinstance(_asset, assets.Video):
+                            projects[_asset.key.parent().id()]['video'] = _asset
 
-                # copy over avatar assets
-                if project.get('avatar'):
-                    if project.get('avatar').get('model'):
-                        asset_id = project.get('avatar').get('model').key.id()
-                    elif isinstance(project.get('avatar'), ndb.Key):
-                        asset_id = project.get('avatar').id()
+                ## build project structs
+                for key_id, project in projects.items():
 
-                    # grab the avatar location
-                    if asset_id in _assets:
+                    # copy over avatar assets
+                    if project.get('avatar'):
+                        if project.get('avatar').get('model'):
+                            asset_id = project.get('avatar').get('model').key.id()
+                        elif isinstance(project.get('avatar'), ndb.Key):
+                            asset_id = project.get('avatar').id()
 
                         # find the asset first
-                        project['avatar']['asset'] = _assets.get(asset_id)
+                        project['avatar']['asset'] = _assets[asset_id]
 
                         # if the avatar record has a URL on it, use that
                         if project['avatar'].get('url'):
@@ -95,8 +106,19 @@ class Landing(WebHandler):
                             extension = project['avatar']['asset'].get('mime').split('/')[1]
                             project['avatar']['location'] = self.url_for('serve-blob-filename', action='serve', asset_key=project['avatar']['asset']['key'].urlsafe(), filename='project-avatar-' + project['model'].get_custom_url() + '.' + extension)
 
-            context['projects'] = projects.values()
+                ## copy projects over to page context
+                context['projects'] = projects.values()
 
+            ## pull activity if there's more than one item to pull
+            if ac > 0:
+                pass
+
+            ## cache in memcache if we have items at all
+            if pc > 0 or ac > 0:
+                ## set cached page context
+                self.api.memcache.set('landing_page_context', context)
+
+        ## render auth/noauth template (@TODO)
         if False:
             self.render('main/landing_noauth.html', **context)
         else:
