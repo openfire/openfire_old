b50d48c4dee27d0713041b7e5d8c114c87f93c5f
Ethan Leland <Ethan.Leland@gmail.com>
2012-08-22 19:02:23 -0700

    Implement autocomplete service api method (OF-156)
    This change adds the autocomplete search service, and includes pipeline
    support for Topic models.
     - Turn on pipeline autostart in config.
     - Activate the search service in config.
     - Add a substring_list to the core indexer API.
     - Post-put pipeline to create autocomplete search indexes for topics.
     - Added a RUNNING_TESTS flag to os.environ while tests are running.
     - Tests for the autocomplete method.

--- app/openfire/tests/services/service_tests.py
+++ app/openfire/tests/services/service_tests.py
@@ -1139,3 +1139,26 @@ class PaymentServiceTestCase(OFTestCase):
             'Failed to return correct response type from payment admin_withdrawal_history service.')
         content = response['response']['content']
         #self.assetTrue(content, 'Failed to return content from the payment admin_withdrawal_history service.')
+
+
+class SearchServiceTestCase(OFTestCase):
+
+    ''' Test cases for the search service. '''
+
+    def test_search_autocomplete_method(self):
+
+        ''' Add a topic to the database then query through the auto complete service. '''
+
+        slug = 'test'
+        db_loader.create_topic(slug=slug)
+        params = {
+            'query': slug,
+            'index': 'topic',
+        }
+        response = self.of_service_test('search', 'autocomplete', params=params)
+        self.assertEqual(response['response']['type'], 'SearchResponse',
+            'Search autocomplete service method failed to return proper response.')
+        self.assertEqual(len(response['response']['content']['ids']), 1,
+            'Failed to return the correct number of topics through autocomplete.')
+        self.assertEqual(response['response']['content']['ids'][0], slug,
+            'Failed to return the correct topic id through autocomplete.')
