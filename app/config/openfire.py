2e86c486e38860a11df293816e6fabf448f445e4
Sam Gammon <sam@openfi.re>
2012-09-25 16:27:07 -0700

    Removed north from template chain and promoted content to __base proper, added OpenGraph integration for projects, that supports the project URL, name, description, and video. Soon it will also support linking the project image with height and width info. More to come.

--- app/config/openfire.py
+++ app/config/openfire.py
@@ -82,7 +82,7 @@ config['openfire.meta'] = {
     'icon': 'https://d2ipw8y1masjpy.cloudfront.net/static/branding/icons/of_favicon_32x32.ico',
     'logo': 'https://d2ipw8y1masjpy.cloudfront.net/static/branding/openfire_transparent_optimized.png',
     'author': 'a collaboration between labor lapsus L3C + momentum labs',
-    'publisher': 'openfire now!',
+    'publisher': 'openfire!',
     'copyright': 'openfire, (c) 2012',
     'robots': 'index,follow',
     'revisit': '7 days',
@@ -106,7 +106,7 @@ config['openfire.meta'] = {
         'determiner': 'a',
         'locale': 'en_US',
         'url': 'https://openfi.re',
-        'site_name': 'openfire',
+        'site_name': 'openfire!',
 
         'location': {
             'latitude': '37.751185',
