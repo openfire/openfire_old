8c68b61b452a6dfe87a9242706315dbe3246ee4e
Sam Gammon <sam@openfi.re>
2012-10-01 10:58:26 -0700

    Added method to User model for resolving 3rd party accounts, added OpenGraph tags to project page.

--- app/openfire/models/user.py
+++ app/openfire/models/user.py
@@ -53,6 +53,16 @@ class User(AppModel):
     email = ndb.KeyProperty('em', indexed=True, repeated=True)
     avatar = ndb.KeyProperty('av')
     images = ndb.KeyProperty('im', repeated=True)
+    accounts = ndb.KeyProperty('ac', repeated=True, indexed=True)
+
+    def get_accounts(self):
+
+        ''' Get the user's attached social accounts. '''
+
+        if len(self.accounts) > 0:
+            return self.accounts
+        else:
+            return None
 
     def has_custom_url(self):
 
